83f68ce37a6bff88d0f08ba3ebf51126eebb2919
AJ <99416087+AJSEUN@users.noreply.github.com>
2022-07-14 14:31:02 +0000

    Check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -4,33 +4,31 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+// Challenge 1
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    var total=0;
+    for (var i in arrayOfNumbers) {
+        total += arrayOfNumbers[i]
+    }
+    console.log(total);
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+sumOfNumbers([123,155,134,205,45])
+
+
+// Challenge 2
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0
+    for (i=0; i< arrayOfNumbers.length; i++){
+        if (arrayOfNumbers[i] % 2 == 0){
+            count ++
+        }
+    }
+    console.log(count)
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+countEvenNumbers([4,5,7,8,14,45,76])
 
 /**
  * Challenge - 3
